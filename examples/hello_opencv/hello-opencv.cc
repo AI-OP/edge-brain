b6c36d237a0b7dc2c6a2b4fc445b39f7b0120f87
Duan-JM <vincent.duan95@bupt.edu.cn>
2021-02-16 17:55:58 +0800

    add hello-opencv to examples

---
+++ examples/hello_opencv/hello-opencv.cc
@@ -0,0 +1,15 @@
+#include <opencv2/opencv.hpp>
+
+int main(int argc, char** arv) {
+
+  const int kImageHeight = 240;
+  const int kImageWidth = 320;
+
+  cv::Mat image(kImageHeight, kImageWidth, CV_8UC3);
+
+  image.setTo(cv::Scalar(0,255,0));
+
+  cv::imwrite("hello_opencv.jpg", image);
+
+  return 0;
+}
