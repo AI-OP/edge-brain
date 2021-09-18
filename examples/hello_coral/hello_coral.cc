2a14a8121b4fdce7961c1a782f8b4b5d85031bc7
Sun Aries <281614085@qq.com>
2021-09-18 10:20:39 +0800

    Update hello_coral.cc

--- examples/hello_coral/hello_coral.cc
+++ examples/hello_coral/hello_coral.cc
@@ -1,5 +1,4 @@
-/* Copyright 2020 The TensorFlow Authors and Sun Aries. 
-All Rights Reserved.
+/* Copyright 2021 The Edge Brain Authors, The TensorFlow Authors.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
